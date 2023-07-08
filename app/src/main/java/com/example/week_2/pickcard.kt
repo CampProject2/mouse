03776bea10aacfc82ebe15992e99c46791edd8b5
seyeoncho33 <135544903+seyeoncho33@users.noreply.github.com>
2023-07-09 03:50:07 +0900

    change

--- app/src/main/java/com/example/week_2/pickcard.kt
+++ app/src/main/java/com/example/week_2/pickcard.kt
@@ -1,7 +1,9 @@
 package com.example.week_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.GridView
+import com.example.week_2.R
 
 class pickcard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
