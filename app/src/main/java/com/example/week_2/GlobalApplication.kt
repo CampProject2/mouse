6cf7654bb8d74fa5bc94ab6f0c380aee7fceb243
hyunsoo <top010301@kaist.ac.kr>
2023-07-08 23:46:40 +0900

    2nd commit

---
+++ app/src/main/java/com/example/week_2/GlobalApplication.kt
@@ -0,0 +1,15 @@
+package com.example.week_2
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val naive_app_key = "2115599024481932941aa97f0c3ebaf9"
+
+        // KaKao SDK  초기화
+        KakaoSdk.init(this, naive_app_key)
+    }
+}
---
+++ app/src/main/java/com/example/week_2/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.week_2
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+//        Log.d("해시키 내놔", getAppKeyHash(this).toString())
+
+        val btnKLogin = findViewById<Button>(R.id.btn_klogin)
+        btnKLogin.setOnClickListener {
+
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("LOGIN", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+
+                    Log.i("LOGIN", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                    val intent = Intent(this@MainActivity, gameset::class.java)
+                    startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                    finish()
+                }
+            }
+
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e("LOGIN", "카카오톡으로 로그인 실패", error)
+
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i("LOGIN", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                        val intent = Intent(this@MainActivity, gameset::class.java)
+                        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                        finish()
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
+    }
+
+//    private fun getAppKeyHash(context: Context): String? {
+//        var appKeyHash: String? = null
+//        try {
+//            val packageInfo = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
+//            for (signature in packageInfo.signatures) {
+//                val md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                appKeyHash = Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            e.printStackTrace()
+//        } catch (e: NoSuchAlgorithmException) {
+//            e.printStackTrace()
+//        }
+//        return appKeyHash
+//    }
+
+}
